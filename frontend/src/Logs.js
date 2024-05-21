20828e2535ff8f91812aa6feccaf7006dae22df6
Michał Jadach <michal.jadach@hoprnet.org>
2024-05-21 15:34:53 +0200

    show your ip

--- frontend/src/Logs.js
+++ frontend/src/Logs.js
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 
 function Logs({serverurl}) {
     const [logs, add_Log] = useState([]);
+    const [myIp, set_myIp] = useState(null);
 
     useEffect(() => {
+        getMyIp();
         joinWebSocket();
-      }, []);
+    }, []);
 
     const joinWebSocket = () => {
         const wsUrl = `wss://${serverurl}/client_logs/websocket`;
@@ -36,13 +38,19 @@ function Logs({serverurl}) {
           const newState = [...prevState, entry];
           return newState;
         });
-      };
+    };
+
+    const getMyIp = async () => {
+        const rez = await fetch('https://ifconfig.me/ip');
+        const ip = await rez.text();
+        set_myIp(ip);
+    };
 
     return (
         <div className="logs">
             {
                 logs.map((log)=>
-                    <Log log={log} />
+                    <Log log={log} myIp={myIp}/>
                 )
             }
         </div>
@@ -62,7 +70,7 @@ function Log(props) {
     return (
         <div className="log">
             <span className="req">REQ >></span><br/>{' '}
-            <span className="req">{'IP: '}</span>{ip}
+            <span className="req">{'IP: '}</span><span className={`${props.myIp === ip ? `myIp` : ''}`}>{ip}</span>
             {' '}<span className="req">{'country: '}</span>{country}
             {' '}<span className="req">{'city: '}</span>{city}
             {' '}<span className="req">{'postal code: '}</span>{postalCode}<br/>
