56126a286f7b40b38bc1470b18f6f482dc6fe24b
Michał Jadach <michal.jadach@hoprnet.org>
2025-09-06 13:17:02 +0200

    add civic login button to the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
+import './civic.css';
 import React from "react";
 import Portfolio from './Portfolio';
 import Logs from './Logs'
-const Fingerprint = React.lazy(() => import('./Fingerprint'));
+import { CivicAuthProvider, UserButton } from "@civic/auth-web3/react";
+import { useUser } from "@civic/auth-web3/react";
 
 function App() {
+  const clientId = process.env.REACT_APP_CIVIC_CLIENT_ID;
+
   return (
-    <div className="App">
-      <Portfolio/>
-      <Logs/>
-      <Fingerprint/>
-    </div>
+    <CivicAuthProvider
+      clientId={clientId}
+    >
+      <div className="App">
+        <UserButton
+          className='civic-user-button'
+        //  dropdownButtonClassName="civic-user-button-dropdown"
+        />
+        <Portfolio/>
+        <Logs/>
+      </div>
+    </CivicAuthProvider>
   );
 }
 
